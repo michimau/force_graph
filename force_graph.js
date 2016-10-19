d98bccb24682dc0fe4c980d3b70ec773ca1d79c5
Mauro Michielon <mauro.michielon@eea.europa.eu>
2016-10-19 23:02:26 +0200

    First commit

---
+++ force_graph.js
@@ -0,0 +1,41 @@
+// Include the angular controller
+require('plugins/force_graph/force_graph_controller');
+require('plugins/force_graph/force_graph.css');
+// The provider function, which must return our new visualization type
+
+function ForceGraphProvider(Private) {
+ var TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
+
+    // Include the Schemas class, which will be used to define schemas
+    var Schemas = Private(require('ui/Vis/Schemas'));
+
+    // Describe our visualization
+
+    return new TemplateVisType({
+        name: 'force_graph', // The internal id of the vis (must be unique)
+        title: 'Force Graph', // The title of the vis, shown to the user
+        description: 'Force Graph chart', // The description of this vis
+        icon: 'fa-network', // The font awesome icon of this visualization
+        // The template, that will be rendered for this visualization
+        template: require('plugins/force_graph/force_graph.html'),
+        // Define the aggregation your visualization accepts
+        schemas: new Schemas([{
+            group: 'metrics',
+            name: 'metric',
+            title: 'Metric',
+            min: 1,
+            aggFilter: ['count'],
+            defaults: [{
+                type: 'count',
+                schema: 'metric'
+            }]
+        }, {
+            group: 'buckets',
+            name: 'force_data',
+            title: 'DateField',
+            aggFilter: 'date_histogram'
+        }])
+    });
+}
+
+require('ui/registry/vis_types').register(ForceGraphProvider);
---
+++ force_graph_controller.js
@@ -0,0 +1,134 @@
+re('ui/modules').get('force_graph');
+
+module.controller('force_graph_controller', function($scope, Private) {
+
+    $scope.$watch('esResponse', function(resp) {
+        if (!resp) {
+            return;
+        }
+
+        var width = 600,
+            height = 400;
+        var color = d3.scale.category20();
+
+        var force = d3.layout.force()
+            .charge(-120)
+            .linkDistance(30)
+            .size([width, height]);
+
+        var svg = d3.select("#force_graph" + $scope.$id).append("svg")
+            .attr("width", width)
+            .attr("height", height);
+        // here i am using the sample data
+        var graph = {
+            "nodes": [{
+                "name": "Node A",
+                "group": 1
+            }, {
+                "name": "Node B",
+                "group": 2
+            }, {
+                "name": "Node C",
+                "group": 1
+            }, {
+                "name": "Node D",
+                "group": 3
+            }, {
+                "name": "Node E",
+                "group": 2
+            }, {
+                "name": "Node F",
+                "group": 1
+            }],
+            "links": [{
+                "source": 1,
+                "target": 0,
+                "value": 1
+            }, {
+                "source": 2,
+                "target": 4,
+                "value": 8
+            }, {
+                "source": 3,
+                "target": 0,
+                "value": 10
+            }, {
+                "source": 4,
+                "target": 3,
+                "value": 6
+            }, {
+                "source": 5,
+                "target": 1,
+                "value": 1
+            }, {
+                "source": 5,
+                "target": 2,
+                "value": 1
+            }]
+        };
+
+        /* 
+         //if you want to use elasticsearch data you can get the data 
+         // as below. you have to convert the data to required format
+         var dataId = $scope.vis.aggs.bySchemaName['force_data'][0].id;
+         var dataBuckets = resp.aggregations[sourceId].buckets;
+         var metricsAgg = $scope.vis.aggs.bySchemaName['metric'][0];
+         dataBuckets.map(function(bucket){
+           var name=bucket.label; // name
+           var value = metricsAgg.getValue(bucket); // count   
+         
+         });
+        */
+
+        force
+            .nodes(graph.nodes)
+            .links(graph.links)
+            .start();
+
+        var link = svg.selectAll(".link")
+            .data(graph.links)
+            .enter().append("line")
+            .attr("class", "link")
+            .style("stroke-width", function(d) {
+                return Math.sqrt(d.value);
+            });
+
+        var node = svg.selectAll(".node")
+            .data(graph.nodes)
+            .enter().append("circle")
+            .attr("class", "node")
+            .attr("r", 5)
+            .style("fill", function(d) {
+                return color(d.group);
+            })
+            .call(force.drag);
+
+        node.append("title")
+            .text(function(d) {
+                return d.name;
+            });
+
+        force.on("tick", function() {
+            link.attr("x1", function(d) {
+                    return d.source.x;
+                })
+                .attr("y1", function(d) {
+                    return d.source.y;
+                })
+                .attr("x2", function(d) {
+                    return d.target.x;
+                })
+                .attr("y2", function(d) {
+                    return d.target.y;
+                });
+
+            node.attr("cx", function(d) {
+                    return d.x;
+                })
+                .attr("cy", function(d) {
+                    return d.y;
+                });
+        });
+
+    });
+});
